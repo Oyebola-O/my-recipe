0e7b1b29574cd54b0b384c88ae4430619568deb8
MOROLARI <tmorolari@gmail.com>
2018-07-30 14:39:33 +0100

    feature(User should be able to login): fix cors error

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,32 @@
 base_url = 'https://more-recipes.herokuapp.com/api/v1';
 
-function signIn(user)
-{
+function signIn(user) {
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+
   loginInfo = {
     user: {
-      email: 'tmorolaari@gmail.com',
+      email: 'tmorolari@gmail.com',
       password: 'password'
     }
-  }
+  };
   let fetchData = {
     method: 'POST',
-    body: loginInfo,
+    body: JSON.stringify(loginInfo),
     headers: {
       'Content-Type': 'application/json'
     },
-
   };
   const url = `${base_url}/users/login`;
   fetch(url, fetchData)
+    .then((res) => res.json())
     .then((data) => {
-    console.log(data)
-  }).catch((err) => {
+      console.log(data);
+      if (typeof(Storage) !== "undefined") {
+        localStorage.token = data.token;
+        location.replace('page.html');
+      }
+    }).catch((err) => {
     console.log('')
   });
 }
