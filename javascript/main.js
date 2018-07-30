d8804289089f59dd642847dabf3e55f52a03c453
jideajayi11 <jideajayi11@gmail.com>
2018-07-30 15:35:36 -0700

    get one recipe

--- javascript/main.js
+++ javascript/main.js
@@ -16,3 +16,22 @@
       console.log(err)
     });
   }
+
+  function getOneRecipe(id) {
+    let request = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': localStorage.token
+      },
+    };
+    
+    const url = 'https://more-recipes.herokuapp.com/api/v1/recipes/' + id;
+    fetch(url, request)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      }).catch((err) => {
+      console.log(err)
+    });
+  }
