f2d5470a42426d39ba38a0c654aec90954351549
jideajayi11 <jideajayi11@gmail.com>
2018-07-30 15:24:02 -0700

    get all recipes

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+  function getRecipes() {
+    let request = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': localStorage.token
+      },
+    };
+    
+    const url = 'https://more-recipes.herokuapp.com/api/v1/recipes';
+    fetch(url, request)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      }).catch((err) => {
+      console.log(err)
+    });
+  }
