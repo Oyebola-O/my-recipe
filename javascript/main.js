2fa732c670db653f743e73e026821f5ef6b6a880
Eneh <erozonachi.james@gmail.com>
2018-07-30 14:44:26 +0100

    Sign up new user via the signup endpoint
    Created a function to signup new user through signup endpoint @
    https://more-recipes.herokuapp.com/api/v1/users/signup with the user's
    full name, email, password.

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+function signUp() {
+  const url = 'https://more-recipes.herokuapp.com/api/v1/users/signup';
+  let data = {
+    user: {
+      fullname: "James Eneh",
+      email: "james@gmail.com",
+      password: "enjd9393md#"
+    }
+  };
+  let fetchData = { 
+    method: 'POST', 
+    body: JSON.stringify(data),
+    headers: {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+  };
+
+  fetch(url, fetchData)
+  .then((resp) => resp.json(), (error) => {
+    console.error(error);
+    alert('SignUp cannot be completed at this time! Try again');
+  })
+  .then((data) => {
+    if (data.status == 'success') {
+      console.log(data);
+    } else {
+      console.log(data);
+    }
+  }, (error) => {
+    console.error(error);
+    alert('SignUp cannot be completed at this time! Try again');
+  })
+  .catch ((error) => {
+    console.error(error);
+    alert('Unable to sign up, try again');
+  });
+}
+signUp();
